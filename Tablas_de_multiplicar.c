f64816774cfa815673e61cc385044868386dc8a4
aibarra9-ux <alantapon26@gmail.com>
2025-12-10 16:28:03 -0700

    Subir cambios

--- Tablas_de_multiplicar.c
+++ Tablas_de_multiplicar.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hola mundo\n");
-    return 0;
+int main(){
+	int i = 1,j = 1, res;
+	
+
+
+
+	while(i<=10)
+	{
+		res = 4 * i;
+		
+		printf("4 x %d = %d \n",i,res);
+
+		i++;
+	}
+
+
+
+	while(j<=10)
+        {
+                res = 5 * j;
+
+                printf("5 x %d = %d \n",j,res);
+
+                j++;
+        }
+
+
+
+	return 0;
 }
